4342d48b0a429a7f33c7d31da60e8d83ac952ba3
Shaurya926 <santoshdubey7089@gmail.com>
2025-11-12 10:46:44 +0530

    12_nov

---
+++ hello_java/lect_5/append.java
@@ -0,0 +1,11 @@
+package lect_5;
+
+public class append {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder();
+        sb.append(" world");
+        System.out.println("Initial: " + sb);
+        System.out.println("a" + sb);
+
+    }
+}
---
+++ hello_java/lect_5/insert.java
@@ -0,0 +1,11 @@
+package lect_5;
+
+public class insert {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder();
+        sb.insert(0, "hello");
+        System.out.println("Initial: " + sb);
+        System.out.println("insert():world "+sb);
+
+    }
+}
