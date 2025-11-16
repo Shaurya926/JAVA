31d615ffd88e7ac257ede1ea7da4f0f1838f94c9
Shaurya926 <santoshdubey7089@gmail.com>
2025-11-16 23:19:14 +0530

    16th_nov_2025

---
+++ hello_java/lect_6/capacity.java
@@ -0,0 +1,10 @@
+package lect_6;
+
+public class capacity {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("hello");
+         System.out.println("Initial: " + sb);
+         System.out.println("capacity(): " + sb.capacity());
+
+    }
+}
