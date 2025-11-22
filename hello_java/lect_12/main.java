1f1ec713806673df5f12e32b9c7c3d410faf04e8
Shaurya926 <santoshdubey7089@gmail.com>
2025-11-23 04:45:01 +0530

    lect_12

---
+++ hello_java/lect_12/main.java
@@ -0,0 +1,35 @@
+package lect_12;
+abstract class shape
+{
+    abstract void area();
+}
+class circle extends shape{
+    int r;
+    public circle(int r){
+        this.r=r;
+    }
+    void area(){
+        double result=r*r;
+        System.out.println("Area of circle is:" +result);
+    }
+}
+class rectangle extends shape{
+    int lenght;
+    int b;
+    public rectangle(int lenght,int b){
+        this.lenght=lenght;
+        this.b=b;
+    }
+    void area(){
+        double result=lenght*b;
+        System.out.println("Area of rectangle is:"+result);
+    }
+}
+public class main {
+    public static void main(String[] args) {
+        circle c=new circle(5);
+        c.area();
+        rectangle r1=new rectangle(14,20);
+        r1.area();
+    }
+}
