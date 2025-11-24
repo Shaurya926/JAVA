79fcc1c614ea7a88b649c06689ad649dd8e8cac6
Shaurya926 <santoshdubey7089@gmail.com>
2025-11-24 15:13:10 +0530

    lect_12

---
+++ hello_java/lect_13/Main.java
@@ -0,0 +1,8 @@
+package lect_13;
+
+public class Main {
+    public static void main(String[] args) {
+        V s1= new V();
+        s1.show();
+    }
+}
---
+++ hello_java/lect_13/V.java
@@ -0,0 +1,7 @@
+package lect_13;
+
+public class V {
+    void show(){
+        System.out.println("Zee");
+    }
+}
