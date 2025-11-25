07fcd1494fc9ded9dbec748d7da0628f2e103d78
Shaurya926 <santoshdubey7089@gmail.com>
2025-11-25 14:47:13 +0530

    lect_13

--- hello_java/Main.java
+++ hello_java/Main.java
@@ -1,8 +1,12 @@
-package lect_13;
+import lect_13.V;
+import lect_13.Z;
 
 public class Main {
     public static void main(String[] args) {
         V s1= new V();
         s1.show();
+        Z s2= new Z();
+        s2.sleep();
+
     }
 }
---
+++ hello_java/lect_13/Z.java
@@ -0,0 +1,7 @@
+package lect_13;
+
+public class Z {
+    public void sleep(){
+        System.out.println("this..");
+    }
+}
