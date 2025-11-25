8dbd41deddef7bd1a6ce1e15b1f5349dbe282035
Shaurya926 <santoshdubey7089@gmail.com>
2025-11-25 15:33:03 +0530

    lect_14

--- hello_java/lect_14/list1.java
+++ hello_java/lect_14/list1.java
@@ -1,12 +1,18 @@
 package lect_14;
+
 import java.util.ArrayList;
-public class Main {
+
+class list1Main {
     public static void main(String[] args) {
         ArrayList<Integer> List =new ArrayList<>();
+        ArrayList<Integer> List1 =new ArrayList<>();
         List.add(5);
         List.add(6);
         List.add(0,22);
         System.out.println(List);
-
+        List1.add(1);
+        List1.add(1,11);
+        List1.addAll(List);
+        System.out.println(List1);
     }
 }
