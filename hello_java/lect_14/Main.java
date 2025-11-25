05a0d9eead5fd091c06325af025ce551f8d7cdfe
Shaurya926 <santoshdubey7089@gmail.com>
2025-11-25 15:15:38 +0530

    lect_14

---
+++ hello_java/lect_14/Main.java
@@ -0,0 +1,12 @@
+package lect_14;
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> List =new ArrayList<>();
+        List.add(5);
+        List.add(6);
+        List.add(0,22);
+        System.out.println(List);
+
+    }
+}
