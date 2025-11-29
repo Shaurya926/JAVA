b5455056a193697aeb23810fb2692d2af42609b0
Shaurya926 <santoshdubey7089@gmail.com>
2025-11-29 23:43:04 +0530

    lect_17

---
+++ hello_java/lect_17/main.java
@@ -0,0 +1,12 @@
+package lect_17;
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(22);
+        set.add(45);
+        set.add(21);
+        System.out.println(set);
+    }
+}
