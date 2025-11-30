4126de4a9ad17c51ac12cc54dc18ebbb1f8cdf2c
Shaurya926 <santoshdubey7089@gmail.com>
2025-11-30 23:02:43 +0530

    lect_17

---
+++ hello_java/lect_17/Loop.java
@@ -0,0 +1,17 @@
+package lect_17;
+import java.util.*;
+
+public class Loop {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        set.add("Red");
+        set.add("Blue");
+        set.add("Green");
+        set.add("Pink");
+        set.add("Yellow");
+
+        for (String s : set) {
+            System.out.println(s);
+        }
+    }
+}
