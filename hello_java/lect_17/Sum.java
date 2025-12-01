5aefc777e608ecbbe2143cf745a20d0e71321205
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-01 23:59:17 +0530

    lect_17

---
+++ hello_java/lect_17/Sum.java
@@ -0,0 +1,19 @@
+package lect_17;
+
+import java.util.*;
+
+public class Sum {
+    public static void main(String[] args) {
+        int ar[]={2,7,5,4};
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int target=12;
+        for(int i=0;i<ar.length;i++){
+            int diff=target-ar[i];
+            if(map.containsKey(diff)){
+                System.out.println(map.get(diff)+" "+i);
+                break;
+            }
+            map.put(ar[i],i);
+        }
+    }
+}
