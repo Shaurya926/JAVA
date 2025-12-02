8b6e8ecf6193833a94023afaa1157ba8a8e70297
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-03 00:09:29 +0530

    Array_List

---
+++ hello_java/Array_List/CAL.java
@@ -0,0 +1,12 @@
+package Array_List;
+import java.util.ArrayList;
+//Create array list.
+public class CAL {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add(1);
+        list.add(6);
+        list.add(1,2);
+        System.out.println(list);
+    }
+}
