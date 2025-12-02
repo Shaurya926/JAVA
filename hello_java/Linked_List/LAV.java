4a382a5e309535a7f1eaeb22e92e337ea044fc44
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-02 23:58:14 +0530

    Linked_List

---
+++ hello_java/Linked_List/LAV.java
@@ -0,0 +1,10 @@
+package Linked_List;
+import java.util.LinkedList;
+public class LAV {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        System.out.println(list);
+    }
+}
