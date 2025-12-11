7d827168a0495b77ac5359fe8bd999c4ab5c4f52
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-11 11:50:20 +0530

    Exception

--- hello_java/lect_19/J1.java
+++ hello_java/lect_19/J1.java
@@ -1,4 +1,20 @@
 package lect_19;
 
 public class J1 {
+    public static void main(String[] args) {
+        try {
+            riskyMethod();
+        } catch (Exception e) {
+            System.out.println("Main me handle hua: " + e);
+        }
+        System.out.println("Program continue...");
+    }
+
+    static void riskyMethod() throws ArithmeticException {
+        System.out.println("riskyMethod start");
+        int result = 10 / 0;
+        System.out.println("Result: " + result);
+        System.out.println("riskyMethod end");
+    }
 }
+
---
+++ hello_java/lect_21/J3.java
@@ -0,0 +1,34 @@
+package lect_21;
+
+import java.util.*;
+
+// File downloading.
+class MyThread2 extends Thread {
+    public void run() {
+        System.out.println("Hello World");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("File Downloading" + i + "0%");
+            try {
+                Thread.sleep(2000);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class J3 {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+        MyThread2 myThread = new MyThread2();
+        myThread.start();
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Complete" + i + "0%");
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
