6d49bf81377cc6acc3dc71c1631693ae42d6b5a0
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-12 13:32:37 +0530

    MultiThreading_DifferentMethod

---
+++ hello_java/lect_22/J1.java
@@ -0,0 +1,16 @@
+package lect_22;
+//Different way to use multi thread.
+class myThread implements Runnable{
+    public void run(){
+        System.out.println("My thread is running");
+    }
+}
+public class J1 {
+    public static void main(String[] args) {
+        myThread task = new myThread();
+        Thread t1= new Thread(task);
+        Thread t2= new Thread(task);
+        t1.start();
+        t2.start();
+    }
+}
