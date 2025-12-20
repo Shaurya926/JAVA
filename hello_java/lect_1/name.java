7cfde1d2afd6e1bf671c9eb938e90aff911fd512
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-20 23:37:37 +0530

    Simple_Java_code_for_beginners

--- hello_java/lect_1/name.java
+++ hello_java/lect_1/name.java
@@ -1,7 +1,20 @@
 package lect_1;
 
 public class name {
-    public static void main(String[] args){
-        System.out.println("My name is Shaurya Dubey");
+   public static void main(String[] args){
+//        - public
+//        - Access modifier → means this method is accessible from anywhere.
+//                - The JVM must be able to call it, so it has to be public.
+//        - static
+//        - Belongs to the class, not to an object.
+//        - JVM doesn’t create an object of your class to run the program; it directly calls this method.
+//                - That’s why main must be static.
+//        - void
+//        - Return type → means this method doesn’t return any value.
+//                - The JVM just executes it and doesn’t expect a result back.
+//                - main
+//                - The special method name recognized by JVM as the starting point of the program.
+//                for example:-
+                System.out.println("My name is Shaurya Dubey");
     }
 }
