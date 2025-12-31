b293efa1f514b05fdd4c19a44704153251d7032d
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-31 23:38:19 +0530

    This_package_contain's_ListIterator_program's

--- hello_java/lect_18/C3.java
+++ hello_java/lect_18/C3.java
@@ -6,39 +6,41 @@ import java.util.ListIterator;
 
 public class C3 {
     public static void main(String[] args) {
-        ArrayList<Integer>list=new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
         list.add(6);
         list.add(3);
         list.add(6);
         list.add(4);
         list.add(7);
         list.add(2);
-        ListIterator<Integer>ot=list.listIterator();
+        ListIterator<Integer> ot = list.listIterator();
 
         while (ot.hasNext()) {
-            Integer x=ot.next();
-            if(x==7){
+            Integer x = ot.next();
+            if (x == 7) {
                 ot.set(5);
                 ot.add(8);
             }
 
         }
-        while (ot.hasPrevious()){
-            Integer x= ot.previous();
-            System.out.println(x);
+        while (ot.hasPrevious()) {
+            System.out.println(ot.previous());
         }
-        while (ot.hasNext()){
-            Integer x= ot.next();
-            System.out.println(x);
+
+        while (ot.hasNext()) {
+            System.out.println(ot.next());
         }
-        Iterator<Integer>it=list.iterator();
-        while(it.hasNext())
-        {
-            if(it.next()==6){
+
+        Iterator<Integer> it = list.iterator();
+        while (it.hasNext()) {
+            Integer val = it.next();
+            if (val == 6) {
                 it.remove();
+            } else {
+                System.out.println(val);
             }
-            System.out.println(it.next());
         }
+
         System.out.println(list);
 
     }
